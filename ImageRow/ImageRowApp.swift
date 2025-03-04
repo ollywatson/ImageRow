2b8cc6f7391d68b8a6eb386ae2ff349ab31dcb84
ollywatson <32039564+ollywatson@users.noreply.github.com>
2025-03-04 08:37:07 +0000

    Initial Commit

---
+++ ImageRow/ImageRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRowApp.swift
+//  ImageRow
+//
+//  Created by Olly Watson on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageRowTests/ImageRowTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRowTests.swift
+//  ImageRowTests
+//
+//  Created by Olly Watson on 04/03/2025.
+//
+
+import Testing
+@testable import ImageRow
+
+struct ImageRowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
