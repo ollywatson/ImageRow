a7e0af304a8b303333b5d0cb0bd401f3fa050662
ollywatson <32039564+ollywatson@users.noreply.github.com>
2025-03-04 13:16:03 +0000

    Added second approach using masked circles

--- ImageRow/ContentView.swift
+++ ImageRow/ContentView.swift
@@ -46,38 +46,134 @@ struct OverlappingCircles: InsettableShape {
 
 struct ContentView: View {
     var body: some View {
-        HStack(spacing: -4) {
-            Image("cupcakes")
-                .resizable()
-                .frame(width: 48, height: 48)
-                .mask(
+        VStack {
+            
+//          Approach 1: using a custom shape as a mask
+            HStack {
+                Text("With custom shape")
+                Spacer()
+            }.padding(.top)
+            HStack(spacing: -4) {
+                Image("cupcakes")
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .mask(
+                        Circle()
+                    )
+                    .overlay(
+                        Circle()
+                            .strokeBorder(.border, lineWidth: 0.5)
+                    )
+                Image("cupcakes")
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .mask(
+                        OverlappingCircles()
+                    )
+                    .overlay(
+                        OverlappingCircles()
+                            .strokeBorder(.border, lineWidth: 0.5)
+                    )
+                Image("cupcakes")
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .mask(
+                        OverlappingCircles()
+                    )
+                    .overlay(
+                        OverlappingCircles()
+                            .strokeBorder(.border, lineWidth: 0.5)
+                    )
+                
+                Spacer()
+            }
+            
+            
+//          Approach 2: using layers of circles to "fake" the effect
+            HStack {
+                Text("With masked circles")
+                Spacer()
+            }.padding(.top)
+            HStack(spacing: -4) {
+                ZStack {
                     Circle()
-                )
-                .overlay(
+                        .fill(.background)
+                        .frame(width: 54, height: 54)
+                    Image("cupcakes")
+                        .resizable()
+                        .frame(width: 48, height: 48)
+                        .mask(
+                            Circle()
+                        )
+                        .overlay(
+                            Circle()
+                                .strokeBorder(.border, lineWidth: 0.5)
+                        )
+                }
+                .frame(width: 48, height: 48)
+                .zIndex(3)
+                
+                ZStack {
+//                  Start by drawing a larger background circle that occludes the next image
+                    Circle()
+                        .fill(.background)
+                        .frame(width: 54, height: 54)
+//                  Draw the image
+                    Image("cupcakes")
+                        .resizable()
+                        .frame(width: 48, height: 48)
+                        .mask(
+                            Circle()
+                        )
+                        .overlay(
+                            Circle()
+                                .strokeBorder(.border, lineWidth: 0.5)
+                        )
+//                  Draw a larger circle that adds the outline on the left hand edge of the shape
                     Circle()
                         .strokeBorder(.border, lineWidth: 0.5)
-                )
-            Image("cupcakes")
-                .resizable()
+                        .frame(width: 54, height: 54)
+//                      Offset the circle to the left hand edge
+                        .offset(x: -43)
+//                      Mask the circle to keep the outline within the main shape
+                        .mask(Circle().frame(width: 47, height: 47))
+                }
                 .frame(width: 48, height: 48)
-                .mask(
-                    OverlappingCircles()
-                )
-                .overlay(
-                    OverlappingCircles()
+//              Add zIndex to place the image above the next images in the sequence
+                .zIndex(2)
+                
+                ZStack {
+//                  Start by drawing a larger background circle that occludes the next image
+                    Circle()
+                        .fill(.background)
+                        .frame(width: 54, height: 54)
+//                  Draw the image
+                    Image("cupcakes")
+                        .resizable()
+                        .frame(width: 48, height: 48)
+                        .mask(
+                            Circle()
+                        )
+                        .overlay(
+                            Circle()
+                                .strokeBorder(.border, lineWidth: 0.5)
+                        )
+//                  Draw a larger circle that adds the outline on the left hand edge of the shape
+                    Circle()
                         .strokeBorder(.border, lineWidth: 0.5)
-                )
-            Image("cupcakes")
-                .resizable()
+                        .frame(width: 54, height: 54)
+//                      Offset the circle to the left hand edge
+                        .offset(x: -43)
+//                      Mask the circle to keep the outline within the main shape
+                        .mask(Circle().frame(width: 47, height: 47))
+                }
                 .frame(width: 48, height: 48)
-                .mask(
-                    OverlappingCircles()
-                )
-                .overlay(
-                    OverlappingCircles()
-                        .strokeBorder(.border, lineWidth: 0.5)
-                )
-        }
+//              Add zIndex to place the image above the next images in the sequence
+                .zIndex(1)
+                
+                Spacer()
+            }
+        }.padding()
     }
 }
 
